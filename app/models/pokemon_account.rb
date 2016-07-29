cb96d05ce2b4c752e45eade84cce69bb4047e12c
Sylvain Kraisin <sylvain.kraisin@gmail.com>
2016-07-29 20:07:16 +0200

    minor

--- app/models/pokemon_account.rb
+++ app/models/pokemon_account.rb
@@ -1,2 +1,12 @@
 class PokemonAccount < ActiveRecord::Base
+  require 'poke-api'
+
+  # after_commit :pkmn_api, on: :create
+  #
+  # def pkmn_api
+  #   client = Poke::API::Client.new
+  #   client.store_location('Paris')
+  #   client.login(self.email, self.encrypted_password, 'google')
+  #   raise
+  # end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,13 +3,4 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-
-  after_commit :pkmn_api, on: :create
-
-  def pkmn_api
-    client = Poke::API::Client.new
-    client.store_location('Paris')
-    client.login(self.email, self.encrypted_password, 'google')
-    raise
-  end
 end
